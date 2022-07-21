d0967f42687e15901eed30f08f87a7d53221ae8a
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-07-21 22:27:27 +0400

    Revert "Connected Swagger 2" This reverts commit 6a7c98bcd8ad1b3b02b5cff55473ee611ef3e56c.
    Added Tomcat
          Json
    Created first HTTP request

--- src/main/java/com/github/Matseonzhek/restaurantrating/web/VoteController.java
+++ src/main/java/com/github/Matseonzhek/restaurantrating/web/VoteController.java
@@ -2,45 +2,45 @@ package com.github.Matseonzhek.restaurantrating.web;
 
 import com.github.Matseonzhek.restaurantrating.model.VoteBoard;
 import com.github.Matseonzhek.restaurantrating.repository.DataJpaVoteBoardRepository;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/api")
 @RestController
-@Api (value = "Vote BoardController")
-public class VoteController {
+@RequestMapping(value = "/rest/votes", produces = MediaType.APPLICATION_JSON_VALUE)
+public  class VoteController {
 
     private final int USER = 10001;
 
     @Autowired
-    private final DataJpaVoteBoardRepository dataJpaVoteBoardRepository;
+    private  DataJpaVoteBoardRepository dataJpaVoteBoardRepository;
 
-    public VoteController(DataJpaVoteBoardRepository dataJpaVoteBoardRepository) {
-        this.dataJpaVoteBoardRepository = dataJpaVoteBoardRepository;
-    }
+//    public VoteController(DataJpaVoteBoardRepository dataJpaVoteBoardRepository) {
+//        this.dataJpaVoteBoardRepository = dataJpaVoteBoardRepository;
+//    }
 
 
 //    public void delete(int id) {
 //        voteBoardRepository.delete(id, USER);
 //    }
+//
+//    public void update(VoteBoard voteBoard, int voteId) {
+//        if (voteBoard.getId() == voteId) {
+////            voteBoardRepository.save(voteBoard, USER);
+//        } else throw new IllegalArgumentException(voteBoard + "must be with id = " + voteId);
+//    }
 
-    public void update(VoteBoard voteBoard, int voteId) {
-        if (voteBoard.getId() == voteId) {
-//            voteBoardRepository.save(voteBoard, USER);
-        } else throw new IllegalArgumentException(voteBoard + "must be with id = " + voteId);
-    }
-
-    //    public VoteBoard create(VoteBoard voteBoard) {
+//    public VoteBoard create(VoteBoard voteBoard) {
 //        return voteBoardRepository.save(voteBoard, USER);
 //    }
-    @GetMapping("/")
-    @ApiOperation(value = "Get all records of Vote Board")
+
+    @GetMapping
     public List<VoteBoard> getAll() {
         return dataJpaVoteBoardRepository.getAll();
     }
